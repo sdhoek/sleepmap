3d404c852807a51a4d08e2ad477f7a33c0c59f21
sjors <s.hoek@geogap.nl>
2018-03-15 11:32:21 +0100

    added leaflet and basemap

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
-
+export class MapComponent implements OnInit, AfterViewInit {
+  private map: L.Map;
+  private kaart: L.TileLayer;
+  
   constructor() { }
 
   ngOnInit() {
+
   }
+
+  ngAfterViewInit() {
+    this.map = new L.Map('map', {
+      center: [52.80330625557036, 6.022953987121583],
+      maxZoom: 24,
+      zoom: 17,
+      attributionControl: false,
+      zoomControl: false
+    });
+    const kaart = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      maxZoom: 24,
+      zIndeX: 0
+    });
+
+    kaart.addTo(this.map);
+  }
+
 
 }
