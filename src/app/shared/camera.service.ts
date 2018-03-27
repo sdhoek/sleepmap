89ebd19eeb0852924c6522ac419cdb620454aecf
sjors <s.hoek@geogap.nl>
2018-03-27 12:19:49 +0200

    changed data loading and added city outlines

--- src/app/shared/camera.service.ts
+++ src/app/shared/camera.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AppHttpService } from './app-http.service';
 
+import * as cityData from '../../assets/city_outlinesv2.json';
+import * as cameraData from '../../assets/camera_all.json';
+import * as viewShedData from '../../assets/viewsheds_25meter_v3.json';
+const anyCityData = cityData as any;
+const anyCameraData = cameraData as any;
+const anyViewShedData = viewShedData as any;
+
 @Injectable()
 export class CameraService {
-
+  public cities = anyCityData;
+  public cameras = anyCameraData;
+  public viewsheds = anyViewShedData;
   constructor(private http: AppHttpService) {
 
   }
