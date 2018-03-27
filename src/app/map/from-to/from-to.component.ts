ada578130650d98d86ecbc3668aa211fe57d12c3
sjors <s.hoek@geogap.nl>
2018-03-27 15:10:18 +0200

    added debounce keyup

--- src/app/map/from-to/from-to.component.ts
+++ src/app/map/from-to/from-to.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subject } from 'rxjs/Subject';
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/debounceTime";
 @Component({
   selector: 'app-from-to',
   templateUrl: './from-to.component.html',
@@ -7,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FromToComponent implements OnInit {
   //Observable.fromEvent(yourInput, 'keyup').debounceTime(1000).subscribe(value => /* */)
-
+  private vanSubject = new Subject();
   constructor() { }
 
   ngOnInit() {
+    this.vanSubject.debounceTime(500).subscribe((event: Event) => {
+      // Initialize search here.
+
+    });
+
+  }
+
+  public onKeyUp(event: Event) {
+    this.vanSubject.next(event);
   }
 
 }
