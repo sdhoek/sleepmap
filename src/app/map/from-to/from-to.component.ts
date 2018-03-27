ea89de3d8c8eb6edcb7b66e103b053f56ac0188e
Steven Ottens <github@minst.net>
2018-03-27 16:09:17 +0200

    suggest shit

--- src/app/map/from-to/from-to.component.ts
+++ src/app/map/from-to/from-to.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppHttpService } from '../../shared/app-http.service';
 import { Subject } from 'rxjs/Subject';
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/debounceTime";
@@ -10,17 +11,41 @@ import "rxjs/add/operator/debounceTime";
 export class FromToComponent implements OnInit {
   //Observable.fromEvent(yourInput, 'keyup').debounceTime(1000).subscribe(value => /* */)
   private vanSubject = new Subject();
+  public vanSuggests = [];
+  private naarSubject = new Subject();
+  public naarSuggests = [];
+  constructor(private http: AppHttpService) {
 
-  constructor() { }
+  }
   ngOnInit() {
     this.vanSubject.debounceTime(500).subscribe((event: Event) => {
       // Initialize search here.
-
+     this.getVanSuggest(event.target.value)
     });
-
+    this.naarSubject.debounceTime(500).subscribe((event: Event) => {
+      // Initialize search here.
+     this.getNaarSuggest(event.target.value)
+    });
   }
 
-  public onKeyUp(event: Event) {
+  public vanUp(event: Event) {
     this.vanSubject.next(event);
+  }
+  public naarUp(event: Event) {
+    this.naarSubject.next(event);
+  }
+
+  public getVanSuggest(van) {
+    const suggestUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?'
+    this.http.get(suggestUrl+'q='+van).then(r=>{
+      this.vanSuggests = r.response.docs;
+    })
+  }
+
+  public getNaarSuggest(naar) {
+    const suggestUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?'
+    this.http.get(suggestUrl+'q='+naar).then(r=>{
+      this.naarSuggests = r.response.docs;
+    })
   }
 }
