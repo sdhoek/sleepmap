feacb2813acf9b1a6fa41d8e46e376a67d66967c
Steven Ottens <github@minst.net>
2018-05-07 11:32:12 +0200

    localised adressen opzoeken

--- src/app/map/from-to/from-to.component.ts
+++ src/app/map/from-to/from-to.component.ts
@@ -3,6 +3,8 @@ import { AppHttpService } from '../../shared/app-http.service';
 import { Subject } from 'rxjs/Subject';
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/debounceTime";
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-from-to',
   templateUrl: './from-to.component.html',
@@ -16,7 +18,10 @@ export class FromToComponent implements OnInit {
   public naarSuggests = [];
   public vanLoc = [];
   public naarLoc = [];
-  constructor(private http: AppHttpService) {
+  public city = 'amsterdam';
+  public suggestUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?fq=gemeentenaam:amsterdam&q=';
+  public locateUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/lookup?id=';
+  constructor(private http: AppHttpService, private ar: ActivatedRoute) {
   
   }
   ngOnInit() {
@@ -28,6 +33,10 @@ export class FromToComponent implements OnInit {
       // Initialize search here.
      this.getNaarSuggest(event.target.value)
     });
+    this.ar.params.subscribe((event: any) => {
+      this.city = event.city;
+      this.suggestUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?fq=gemeentenaam:'+event.city+'&q='
+    })
   }
 
   public vanUp(event: Event) {
@@ -38,22 +47,19 @@ export class FromToComponent implements OnInit {
   }
 
   public getVanSuggest(van) {
-    const suggestUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?q='
-    this.http.get(suggestUrl+van).then(r=>{
+    this.http.get(this.suggestUrl+van).then(r=>{
       this.vanSuggests = r.response.docs;
     })
   }
 
   public getNaarSuggest(naar) {
-    const suggestUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/suggest?q='
-    this.http.get(suggestUrl+naar).then(r=>{
+    this.http.get(this.suggestUrl+naar).then(r=>{
       this.naarSuggests = r.response.docs;
     })
   }
 
   public locateNaar(item) {
-    const locateUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/lookup?id='
-    this.http.get(locateUrl+item.id).then(r=>{
+    this.http.get(this.locateUrl+item.id).then(r=>{
       let pt = r.response.docs[0].centroide_ll;
       this.naarLoc = [parseFloat(pt.split('(')[1].split(' ')[0]),parseFloat(pt.split('(')[1].split(' ')[1].split(')')[0])]
       this.naarSuggests = [];
@@ -61,8 +67,7 @@ export class FromToComponent implements OnInit {
   }
 
   public locateVan(item) {
-    const locateUrl = 'https://geodata.nationaalgeoregister.nl/locatieserver/v3/lookup?id='
-    this.http.get(locateUrl+item.id).then(r=>{
+    this.http.get(this.locateUrl+item.id).then(r=>{
       let pt = r.response.docs[0].centroide_ll;
       this.vanLoc = [parseFloat(pt.split('(')[1].split(' ')[0]),parseFloat(pt.split('(')[1].split(' ')[1].split(')')[0])]
       this.vanSuggests = [];
