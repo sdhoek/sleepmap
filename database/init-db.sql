c8d4d9d673eb4c28fef15bdf2103ab0d739d0e98
Edward Mac Gillavry <edward@webmapper.net>
2018-12-20 22:28:13 +0100

    Data model created on initialisation. Cameras snap to building edge.

--- database/init-db.sql
+++ database/init-db.sql
@@ -1,2 +1,18 @@
 DROP SCHEMA IF EXISTS civity_imp CASCADE;
 CREATE SCHEMA IF NOT EXISTS civity_imp;
+DROP SCHEMA IF EXISTS civity CASCADE;
+CREATE SCHEMA IF NOT EXISTS civity;
+
+CREATE TABLE civity.cameras (
+    id character varying NOT NULL,
+    bron character varying,
+    doel character varying,
+    eigenaar character varying,
+    kijkrichting character varying,
+    camera_type character varying,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    geom public.geometry(Point,4326),
+    CONSTRAINT cameras_pkey PRIMARY KEY (id)
+);
+
+CREATE INDEX cameras_gdx ON civity.cameras USING GIST (geom);
--- database/transform_points.sql
+++ database/transform_points.sql
@@ -1,17 +1,73 @@
-SELECT 
-	cam.id,
-	cam.bron,
-	cam.gemeente,
-	cam.type,
-	cam.eigenaar,
-	cam.geom,
-	(
-		SELECT ST_Transform(ST_ClosestPoint(ST_ExteriorRing(pand.geovlak), ST_Transform(cam.geom,28992)), 4326)
-    	FROM bag.pand as pand 
-    	WHERE ST_Within(ST_Transform(cam.geom,28992), pand.geovlak)
-    	ORDER BY ST_Transform(cam.geom,28992) <-> ST_ExteriorRing(pand.geovlak)
-    	LIMIT 1
-	) as closespointonpandgeom,
-	ST_Transform(ST_Force2D(pand.geovlak), 4326) as pandgeom
-FROM cameras as cam
-INNER JOIN bag.pand as pand ON ST_Within(ST_Transform(cam.geom,28992), pand.geovlak)
+-- Optimise table
+
+ALTER TABLE civity_imp.cameras DROP COLUMN ogc_fid ;
+ALTER TABLE civity_imp.cameras ADD PRIMARY KEY (id);
+
+CREATE INDEX cameras_geohash ON civity_imp.cameras (ST_GeoHash(geom,10) COLLATE "C");
+CLUSTER civity_imp.cameras USING cameras_geohash;
+DROP INDEX civity_imp.cameras_geohash;
+CREATE INDEX cameras_gdx ON civity_imp.cameras USING GIST (geom);
+
+-- Snap cameras on buildings to nearest edge
+
+WITH updatefeatures AS (
+    SELECT 
+        cam.id,
+        (
+            SELECT
+                ST_Transform(ST_ClosestPoint(ST_ExteriorRing(pand.geovlak), ST_Transform(cam.geom,28992)), 4326)
+            FROM
+                bag.pandactueelbestaand AS pand 
+            WHERE
+                ST_Within(ST_Transform(cam.geom,28992), pand.geovlak)
+            ORDER BY
+                ST_Transform(cam.geom,28992) <-> ST_ExteriorRing(pand.geovlak)
+            LIMIT 1
+        ) AS closespointonpandgeom
+    FROM
+        civity_imp.cameras AS cam
+    INNER JOIN
+        bag.pandactueelbestaand AS pand
+    ON
+        ST_Within(ST_Transform(cam.geom,28992), pand.geovlak)
+)
+UPDATE
+    civity_imp.cameras
+SET
+    geom = updatefeatures.closespointonpandgeom
+FROM
+    updatefeatures
+WHERE
+    civity_imp.cameras.id = updatefeatures.id;
+
+-- Drop index from full table before insert for speed
+
+DROP INDEX civity.cameras_gdx;
+
+-- Insert new locations into the full table
+
+INSERT INTO
+    civity.cameras (id,bron,doel,eigenaar,kijkrichting,camera_type,geom)
+SELECT
+    id,
+    bron,
+    doel,
+    eigenaar,
+    kijkrichting,
+    camera_type,
+    geom
+FROM
+    civity_imp.cameras
+WHERE
+    id NOT IN (SELECT id FROM civity.cameras);
+
+-- Re-oder and put index back on the full table
+
+CREATE INDEX cameras_geohash ON civity.cameras (ST_GeoHash(geom,10) COLLATE "C");
+CLUSTER civity.cameras USING cameras_geohash;
+DROP INDEX civity.cameras_geohash;
+CREATE INDEX cameras_gdx ON civity.cameras USING GIST (geom);
+
+-- Remove import table
+
+DROP TABLE civity_imp.cameras CASCADE;
